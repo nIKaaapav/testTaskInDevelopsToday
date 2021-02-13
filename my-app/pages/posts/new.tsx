391381060d434912c6b708000e4751f8e733d2b4
Nika Pavlenko <nikapavlenko20.np@gmail.com>
2021-02-14 00:10:37 +0200

    create-post|add created post

--- my-app/pages/posts/new.tsx
+++ my-app/pages/posts/new.tsx
@@ -1,20 +1,51 @@
-import React, {useRef} from 'react';
+import React, { useRef } from "react";
+import {useRouter} from "next/router";
 
-const New = () => {
-    const titlePost = useRef();
-    const bodyPost = useRef();
+interface FormData {
+  title: string;
+  body: string;
+}
 
-    return (
-        <div>
-            <h2>new post</h2>
+const New: React.FC = () => {
+  const router = useRouter();
+  const titlePost = useRef<HTMLInputElement>(null);
+  const bodyPost = useRef<HTMLTextAreaElement>(null);
 
-            <form action="" >
-                <input type="text" ref={titlePost} placeholder='title'/>
-                <textarea name="body-post" ref={bodyPost} placeholder='text'/>
-                <button>create</button>
-            </form>
-        </div>
-    );
+  const handleSubmitPost = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const titleData : string = titlePost.current!.value;
+    const bodyData : string = bodyPost.current!.value;
+
+
+
+    if (!bodyData || !titleData) return;
+
+    const data : FormData = {
+      title: titleData,
+      body: bodyData
+    };
+
+    const res = await fetch("https://simple-blog-api.crew.red/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if(!!res) router.push('/')
+  };
+
+  return (
+    <div>
+      <h2>new post</h2>
+      <form action="" onSubmit={handleSubmitPost}>
+        <input type="text" ref={titlePost} placeholder="title" />
+        <textarea name="body-post" ref={bodyPost} placeholder="text" />
+        <button>create</button>
+      </form>
+    </div>
+  );
 };
 
 export default New;
